2eec29421d4aae19a77d144f67ca391740e7540e
Henrik Roos <henrik@afternoon.se>
2015-05-14 22:21:45 +0200

    New version, tested for wp 4.2.2

--- imdb-markup-syntax.php
+++ imdb-markup-syntax.php
@@ -18,7 +18,7 @@ require_once 'callback-management.php';
  * Plugin Name: IMDb Markup Syntax
  * Plugin URI: http://wordpress.org/plugins/imdb-markup-syntax/
  * Description: Add <strong>IMDb</strong> syntax functionallity in your post. Enter simple tags and this plugin replace with IMBb data direct from <a href="http://app.imdb.com">IMDb Mobile Applications</a>
- * Version: 2.4
+ * Version: 2.5
  * Author: Henrik Roos
  * Author URI: http://www.linkedin.com/pub/henrik-roos/28/148/348
  * License: GPL-3.0
